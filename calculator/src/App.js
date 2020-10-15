eab6d2397fb455a29171668ba5241bcf610e816f
rafahg <r.hgar78@gmail.com>
2020-10-15 12:47:49 +0100

    Buttom component created

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import './App.css';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="app">
-      "Hello man!"
+      <div className="calc-wrapper">
+        <div className="row">
+          <Button>7</Button>
+          <Button>8</Button>
+          <Button>9</Button>
+          <Button>/</Button>
+        </div>
+        <div className="row">
+        <Button>4</Button>
+        <Button>5</Button>
+        <Button>6</Button>
+        <Button>x</Button>
+        </div>
+        <div className="row">
+        <Button>1</Button>
+        <Button>2</Button>
+        <Button>3</Button>
+        <Button>+</Button>
+        </div>
+        <div className="row">
+        <Button>.</Button>
+        <Button>0</Button>
+        <Button>=</Button>
+        <Button>-</Button>
+        </div>
+      </div>
     </div>
   
   );
